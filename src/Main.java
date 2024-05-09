abbe1d9b642159559850e9c78c78ac9cf277eefc
february1984 <february.1984@mail.ru>
2024-05-09 04:01:34 +0300

    В функцию getLines добавлен функционал по считыванию данных за весь год. Добавлена новая функция yearReport для составления годового отчета.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String command = "Start";
-        ArrayList<String> fileLines = new ArrayList<>();
-        ArrayList<String> mRep = new ArrayList<>();
+        ArrayList<String> fileLines = new ArrayList<>();//Список для хранения данных из файлов
+        ArrayList<String> finalRep = new ArrayList<>();//Список для хранения отчетов
 
         while (!command.equals("E")){
             System.out.println("Введите команду:\t" +
@@ -26,9 +26,12 @@ public class Main {
                 System.out.println("Пожалуйста введите номер месяца:");
                     String month = scanner.nextLine();
                     fileLines = getLines(month);//Считываем файл
-                    mRep = monthReport(fileLines, month);
+                    finalRep = monthReport(fileLines, month);
             }
-
+            else if (command.equals("Y")){
+                fileLines = getLines("0");//Считываем файл
+                finalRep = yearReport(fileLines);
+            }
         }
     }
     private static String readFiles(String path){
@@ -40,28 +43,46 @@ public class Main {
     }
     private static ArrayList<String> getLines(String month){
         ArrayList<String> result = new ArrayList<>();
-        if (Integer.parseInt(month) <  10){
-            String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.20240"+month+".csv");
-            if (file != null) {
-                String[] lines = file.split("\\n");
-                result.addAll(Arrays.asList(lines).subList(1, lines.length));
+        if (!month.equals("0")) {
+            if (Integer.parseInt(month) < 10) {
+                String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.20240" + month + ".csv");
+                if (file != null) {
+                    String[] lines = file.split("\\n");
+                    result.addAll(Arrays.asList(lines).subList(1, lines.length));
+                }
             }
-        }
-        if (Integer.parseInt(month) >=  10) {
-            String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.2024"+month+".csv");
-            if (file != null) {
-                String[] lines = file.split("\\n");
-                result.addAll(Arrays.asList(lines).subList(1, lines.length));
+            if (Integer.parseInt(month) >= 10) {
+                String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.2024" + month + ".csv");
+                if (file != null) {
+                    String[] lines = file.split("\\n");
+                    result.addAll(Arrays.asList(lines).subList(1, lines.length));
+                }
+            }
+        } else {
+            for(int i = 0; i < 10; i++){
+                String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.20240" + i + ".csv");
+                if (file != null) {
+                    String[] lines = file.split("\\n");
+                    for (int j = 0; j < lines.length; j++){
+                        lines[j]=i + "," + lines[j];
+                    }
+                    result.addAll(Arrays.asList(lines).subList(1, lines.length));
+                }
+            }
+            for(int i = 10; i < 13; i++){
+                String file = readFiles("C:\\Users\\Admin\\IdeaProjects\\sprint1\\files\\m.2024" + i + ".csv");
+                if (file != null) {
+                    String[] lines = file.split("\\n");
+                    for (int j = 0; j < lines.length; j++){
+                        lines[j]=i + "," + lines[j];
+                    }
+                    result.addAll(Arrays.asList(lines).subList(1, lines.length));
+                }
             }
         }
         return result;
     }
     private static void showFile(ArrayList<String> file){
-        for (String value : file) {
-            System.out.println(value);
-        }
-    }
-    private static void showArray(String[] file){
         for (String value : file) {
             System.out.println(value);
         }
@@ -87,6 +108,31 @@ public class Main {
                     }
             }
             result.set(result.indexOf(currResultLine),currResultLine + " " + sum + " " + cost);
+        }
+        for (String curr :result){
+            System.out.println(curr);
+        }
+        return result;
+    }
+    private static ArrayList<String> yearReport(ArrayList<String> fileLines){
+        ArrayList<String> result = new ArrayList<>();//Результирующий список с отчетом
+        String writeToReport = "0";//Строка для обработки записи в отчет
+        for (String fileLine : fileLines) {//Запись в список уникальных пар месяц + Тип транзакции
+            String[] readLine = fileLine.split(",");
+            writeToReport = readLine[0] + "," + readLine[2];
+            if (!result.contains(writeToReport)) {
+                result.add(writeToReport);
+            }
+        }
+        for (String currResultLine : result) {//Проходимся по уникальным парам и плюсуем в них соответвующие транзакции
+            double sum = 0;
+            for (String fileLine : fileLines) {
+                String[] readLine = fileLine.split(",");
+                if (currResultLine.equals(readLine[0] + "," + readLine[2])) {
+                    sum += Double.parseDouble(readLine[3])*Double.parseDouble(readLine[4]);
+                }
+            }
+            result.set(result.indexOf(currResultLine),currResultLine + "," + sum);
         }
         for (String curr :result){
             System.out.println(curr);
